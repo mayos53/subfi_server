68f3a7e02223752ca9f9ee912e8dfe2b3e993e68
mayos53 <mayos53@b7607b4f-956c-4803-aad8-ef715e4fcf39>
2014-10-21 06:16:33 +0000

    Type commit message here…

--- wallpaper/app/controllers/groups_controller.rb
+++ wallpaper/app/controllers/groups_controller.rb
@@ -121,7 +121,7 @@ class GroupsController < ApplicationController
     request = Net::HTTP::Post.new(uri.request_uri)
     
     registration_ids = []
-    @group.memberships.zip(group.users).each do |membership, user|
+    @group.memberships.zip(@group.users).each do |membership, user|
       if user.registrationId != nil and membership.status == 1
           registration_ids <<  user.registrationId
       end
