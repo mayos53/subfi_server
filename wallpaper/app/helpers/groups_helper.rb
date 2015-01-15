f96545a73c1a5ebdf9338a3c6e6fb2b9916575fc
mayos53 <mayos53@b7607b4f-956c-4803-aad8-ef715e4fcf39>
2015-01-15 12:33:54 +0000

    Type commit message here…

--- wallpaper/app/helpers/groups_helper.rb
+++ wallpaper/app/helpers/groups_helper.rb
@@ -29,9 +29,13 @@ def get_group_full_details(group)
     users << @full_user
   end
 
-  #sort
-
   image_time = get_group_image_and_time(group)
+
+  #sort
+  if group.wallpapers != nil and group.wallpapers.exists?
+     wallpapers = group.wallpapers.sort_by{|e| -e.timeSec}
+  end   
+
  
   wallpapers = group.wallpapers.map{|wallpaper| 
     {id: wallpaper.id,path: get_wallpaper_path(wallpaper,:medium),user: wallpaper.user,timeSec: wallpaper.timeSec,title: wallpaper.title}}
@@ -46,11 +50,9 @@ end
 def get_group_image_and_time(group)
   image = nil
   time = nil
-  wallpapers = [];
   if group.wallpapers != nil and group.wallpapers.exists?
-    wallpapers = group.wallpapers.sort_by{|e| -e.timeSec}
-    image = get_wallpaper_path(wallpapers.first,:medium)
-    time = wallpapers.first.timeSec 
+    image = get_wallpaper_path(group.wallpapers.last,:medium)
+    time = group.wallpapers.last.timeSec 
   end  
   return {:image => image, :time => time}
 
