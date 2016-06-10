509b007646246beb62f837bc2c26e082e40e7625
mayos53 <yossi.assayag@gmail.com>
2016-06-10 18:20:15 +0300

    upgrade

--- db/migrate/20140922085206_remove_old_from_wallpapers.rb
+++ db/migrate/20140922085206_remove_old_from_wallpapers.rb
@@ -1,5 +1,5 @@
 class RemoveOldFromWallpapers < ActiveRecord::Migration
   def change
-    remove_column :wallpapers, :field_name, :time
+    remove_column :wallpapers, :time, :timestamp
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -13,111 +13,110 @@
 
 ActiveRecord::Schema.define(version: 20150322211340) do
 
-  create_table "events", force: true do |t|
-    t.integer  "user_id"
-    t.integer  "group_id"
-    t.integer  "time"
-    t.integer  "event_type"
+  create_table "events", force: :cascade do |t|
+    t.integer  "user_id",    limit: 4
+    t.integer  "group_id",   limit: 4
+    t.integer  "time",       limit: 4
+    t.integer  "event_type", limit: 4
     t.datetime "created_at"
     t.datetime "updated_at"
   end
 
-  create_table "groups", force: true do |t|
-    t.string "name"
+  create_table "groups", force: :cascade do |t|
+    t.string "name", limit: 255
   end
 
-  create_table "invitations", force: true do |t|
-    t.integer  "administrator_id"
-    t.integer  "group_id"
-    t.integer  "user_id"
+  create_table "invitations", force: :cascade do |t|
+    t.integer  "administrator_id", limit: 4
+    t.integer  "group_id",         limit: 4
+    t.integer  "user_id",          limit: 4
     t.datetime "created_at"
     t.datetime "updated_at"
   end
 
-  create_table "memberships", force: true do |t|
-    t.integer "user_id"
-    t.integer "group_id"
+  create_table "memberships", force: :cascade do |t|
+    t.integer "user_id",       limit: 4
+    t.integer "group_id",      limit: 4
     t.boolean "administrator"
-    t.integer "status"
+    t.integer "status",        limit: 4
   end
 
-  create_table "push_configurations", force: true do |t|
-    t.string   "type",                        null: false
-    t.string   "app",                         null: false
-    t.text     "properties"
-    t.boolean  "enabled",     default: false, null: false
-    t.integer  "connections", default: 1,     null: false
+  create_table "push_configurations", force: :cascade do |t|
+    t.string   "type",        limit: 255,                   null: false
+    t.string   "app",         limit: 255,                   null: false
+    t.text     "properties",  limit: 65535
+    t.boolean  "enabled",                   default: false, null: false
+    t.integer  "connections", limit: 4,     default: 1,     null: false
     t.datetime "created_at"
     t.datetime "updated_at"
   end
 
-  create_table "push_feedback", force: true do |t|
-    t.string   "app",                          null: false
-    t.string   "device",                       null: false
-    t.string   "type",                         null: false
-    t.string   "follow_up",                    null: false
-    t.datetime "failed_at",                    null: false
-    t.boolean  "processed",    default: false, null: false
+  create_table "push_feedback", force: :cascade do |t|
+    t.string   "app",          limit: 255,                   null: false
+    t.string   "device",       limit: 255,                   null: false
+    t.string   "type",         limit: 255,                   null: false
+    t.string   "follow_up",    limit: 255,                   null: false
+    t.datetime "failed_at",                                  null: false
+    t.boolean  "processed",                  default: false, null: false
     t.datetime "processed_at"
-    t.text     "properties"
+    t.text     "properties",   limit: 65535
     t.datetime "created_at"
     t.datetime "updated_at"
   end
 
-  add_index "push_feedback", ["processed"], name: "index_push_feedback_on_processed"
+  add_index "push_feedback", ["processed"], name: "index_push_feedback_on_processed", using: :btree
 
-  create_table "push_messages", force: true do |t|
-    t.string   "app",                               null: false
-    t.string   "device",                            null: false
-    t.string   "type",                              null: false
-    t.text     "properties"
-    t.boolean  "delivered",         default: false, null: false
+  create_table "push_messages", force: :cascade do |t|
+    t.string   "app",               limit: 255,                   null: false
+    t.string   "device",            limit: 255,                   null: false
+    t.string   "type",              limit: 255,                   null: false
+    t.text     "properties",        limit: 65535
+    t.boolean  "delivered",                       default: false, null: false
     t.datetime "delivered_at"
-    t.boolean  "failed",            default: false, null: false
+    t.boolean  "failed",                          default: false, null: false
     t.datetime "failed_at"
-    t.integer  "error_code"
-    t.string   "error_description"
+    t.integer  "error_code",        limit: 4
+    t.string   "error_description", limit: 255
     t.datetime "deliver_after"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
 
-  add_index "push_messages", ["delivered", "failed", "deliver_after"], name: "index_push_messages_on_delivered_and_failed_and_deliver_after"
+  add_index "push_messages", ["delivered", "failed", "deliver_after"], name: "index_push_messages_on_delivered_and_failed_and_deliver_after", using: :btree
 
-  create_table "recommendations", force: true do |t|
-    t.integer  "user_id"
-    t.string   "user_name"
-    t.integer  "group_id"
-    t.string   "group_name"
-    t.integer  "recommender_id"
-    t.string   "recommender_name"
+  create_table "recommendations", force: :cascade do |t|
+    t.integer  "user_id",          limit: 4
+    t.string   "user_name",        limit: 255
+    t.integer  "group_id",         limit: 4
+    t.string   "group_name",       limit: 255
+    t.integer  "recommender_id",   limit: 4
+    t.string   "recommender_name", limit: 255
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.integer  "administrator_id"
+    t.integer  "administrator_id", limit: 4
   end
 
-  create_table "users", force: true do |t|
-    t.string "name"
-    t.text   "countryCode"
-    t.text   "phone"
-    t.text   "code"
-    t.string "email"
-    t.string "registrationId"
+  create_table "users", force: :cascade do |t|
+    t.string "name",           limit: 255
+    t.text   "countryCode",    limit: 65535
+    t.text   "phone",          limit: 65535
+    t.text   "code",           limit: 65535
+    t.string "email",          limit: 255
+    t.string "registrationId", limit: 255
   end
 
-  create_table "wallpapers", force: true do |t|
-    t.integer  "status"
-    t.datetime "time"
+  create_table "wallpapers", force: :cascade do |t|
+    t.integer  "status",             limit: 4
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.integer  "group_id"
-    t.string   "photo_file_name"
-    t.string   "photo_content_type"
-    t.integer  "photo_file_size"
+    t.integer  "group_id",           limit: 4
+    t.string   "photo_file_name",    limit: 255
+    t.string   "photo_content_type", limit: 255
+    t.integer  "photo_file_size",    limit: 4
     t.datetime "photo_updated_at"
-    t.string   "user_id"
-    t.integer  "timeSec"
-    t.string   "title"
+    t.string   "user_id",            limit: 255
+    t.integer  "timeSec",            limit: 4
+    t.string   "title",              limit: 255
   end
 
 end
