b4b6564d34faf593c53ed5a5a691c0c07fd5f165
mayos53 <yossi.assayag@gmail.com>
2016-06-12 22:44:10 +0300

    rr

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -49,6 +49,7 @@ class GroupsController < ApplicationController
 
    def groups_by_user
      
+      #
       @groups_temp = Group.all(:include => {:memberships => :user}, :conditions => ['users.id=?', params[:id]])
       @groups = Group.includes([{:wallpapers=> :user},{:memberships => :user},:recommendations,{:events => :user}]).where(:id => @groups_temp.map{|group| group.id})
 
